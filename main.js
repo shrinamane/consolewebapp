3a3f175e0e3ad58db83eeca5c1e6503b6b814258
Shrina Surve <88468009+shrinamane@users.noreply.github.com>
2021-11-07 11:28:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var name1="Shrina "
+var name2="Surve"
+var fullname=name1.concat(name2)
+console.log(fullname)
+var name3= "Saphira"
+var up=name3.toUpperCase()
+console.log(up);
+var name4= "Arya Angela"
+var replace=name4.replace("Angela","Katrina")
+console.log(replace) 
+var number1 = 4
+var number2= 1
+console.log(number1+number2)
+var sqareroot=Math.sqrt(256)
+console.log(sqareroot)
+var max=Math.max (15,10)
+console.log(max)
+console.log(number1-number2)
+console.log(number1*number2)
+console.log(number1/number2)
